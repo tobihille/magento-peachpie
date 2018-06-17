7bf27404c3d46665991196e14f50a1b8df2a30a9
Tobias Hille <tobihille@gmail.com>
2018-06-17 13:06:05 +0200

    started fixing webserver

--- webserver/Magento.cs
+++ webserver/Magento.cs
@@ -10,16 +10,24 @@ namespace peachserver
 {
     class Magento {
         static void Main() {
-            var root = Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()) + "/website";
-        
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseWebRoot(root).UseContentRoot(root) // content root with wp static files
-                .UseUrls("http://*:5004/")
-                .UseStartup<Startup>() // initialization routine, see below
-                .Build();
-        
-            host.Run();
+            try {
+                //var root = Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()) + "/website";
+                var root = "C:/Users/tobih/Documents/webroot/hackathon_m1/website/";
+            
+                var host = new WebHostBuilder()
+                    .UseKestrel()
+                    //.UseWebRoot(root)
+                    //content root with static files
+                    //.UseContentRoot(root) 
+                    .UseUrls("http://*:5004/")
+                    //initialization routine, see below
+                    .UseStartup<Startup>() 
+                    .Build();
+            
+                host.Run();
+            } catch (ex) {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
     
@@ -28,12 +36,12 @@ namespace peachserver
             Pchp.Core.Context.DefaultErrorHandler = new Pchp.Core.CustomErrorHandler(); // disables debug asserts
     
             // enable session:
-            app.UseSession();
+            //app.UseSession();
 
-            app.UsePhp(); // installs handler for *.php files and forwards them to our website.dll
+            app.UsePhp( new PhpRequestOptions() { ScriptAssembliesName = new[] { "magento" } } ); // installs handler for *.php files and forwards them to our website.dll
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
         }
     }
 }
