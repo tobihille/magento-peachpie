546e67f7a94621ab12e391058ca35b1b9d719562
Tobias Hille <tobihille@gmail.com>
2018-06-17 11:15:12 +0200

    fixed magento.cs following the examples from https://github.com/iolevel/peachpie-samples/tree/master/web-application

--- webserver/Magento.cs
+++ webserver/Magento.cs
@@ -1,22 +1,39 @@
-static void Main() {
-    var root = Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()) + "/website";
- 
-    var host = new WebHostBuilder()
-        .UseKestrel()
-        .UseWebRoot(root).UseContentRoot(root) // content root with wp static files
-        .UseUrls("http://*:5004/")
-        .UseStartup<Startup>() // initialization routine, see below
-        .Build();
- 
-    host.Run();
-}
- 
-class Startup {
-    public void Configure(IApplicationBuilder app) {
-        Pchp.Core.Context.DefaultErrorHandler = new Pchp.Core.CustomErrorHandler(); // disables debug asserts
- 
-        app.UsePhp(); // installs handler for *.php files and forwards them to our website.dll
-        app.UseDefaultFiles();
-        app.UseStaticFiles();
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.DependencyInjection;
+using Peachpie.Web;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace peachserver
+{
+    class Magento {
+        static void Main() {
+            var root = Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()) + "/website";
+        
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseWebRoot(root).UseContentRoot(root) // content root with wp static files
+                .UseUrls("http://*:5004/")
+                .UseStartup<Startup>() // initialization routine, see below
+                .Build();
+        
+            host.Run();
+        }
+    }
+    
+    class Startup {
+        public void Configure(IApplicationBuilder app) {
+            Pchp.Core.Context.DefaultErrorHandler = new Pchp.Core.CustomErrorHandler(); // disables debug asserts
+    
+            // enable session:
+            app.UseSession();
+
+            app.UsePhp(); // installs handler for *.php files and forwards them to our website.dll
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
     }
 }
